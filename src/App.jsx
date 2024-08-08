26c1b31b3f6eae72f0a88b8ff7008a95097db20b
Pedrinho01505 <pedrinhoavare18@gmail.com>
2024-08-08 19:12:48 -0300

    Iniciando o projeto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  return (
+    <section>
+      <h1>Materiais</h1>
+      <div>
+        
+      </div>
+    </section>
+  )
+}
+
+export default App
