045ae142805e81fe51c2ffdfe4a3415a730bfbd2
Pedro Henrique  Gomes <pedrinhoavare18@gmail.com>
2024-08-14 17:28:27 +0000

    border radius

--- src/pages/Materiais.jsx
+++ src/pages/Materiais.jsx
@@ -19,7 +19,7 @@ function Materiais() {
           <h2 className='text-titulo font-semibold pt-top text-xl'>Arduino</h2>
           <img src={arduino} alt="imagem de uma peça arduino" className='my-inline  md:h-[7rem] md:w-[10rem]'/>
         </div>
-        <div className='bg-bgcaixa rounded-r-[.92rem] flex flex-col items-center md:flex md:flex-col md:justify-center md:max-w-[32.5rem] md:py-desk md:px-desk'>
+        <div className='bg-bgcaixa rounded-b-[.92rem] flex flex-col items-center md:rounded-r-[.92rem] md:rounded-l-none md:flex md:flex-col md:justify-center md:max-w-[32.5rem] md:py-desk md:px-desk'>
           <p className='mb-bottom pt-top flex text-justify p-inline'>Arduino é uma plataforma de computação open-source baseado em uma simples placa com entradas e saídas tanto digitais como analógicas. Possui um próprio ambiente de desenvolvimento que implementa a Linguagem C. O Ambiente de desevolvimento (IDE) open-souce pode ser obtido gratuitamente (atualmente disponível para Mac OS X, Windows, e Linux).</p>
           <p className='pb-top'>Preço: R$ 249,00</p>
         </div>
@@ -31,7 +31,7 @@ function Materiais() {
           <h2 className='text-titulo font-semibold pt-top text-xl md:text-center'>Célula de carga</h2>
           <img src={clcarga} alt="imagem de uma célula de carga" className='my-inline '/>
         </div>
-        <div className='bg-bgcaixa rounded-r-[.92rem] flex flex-col items-center md:flex md:flex-col md:justify-center md:max-w-[32.5rem] md:py-desk md:px-desk'>
+        <div className='bg-bgcaixa rounded-b-[.92rem] flex flex-col items-center md:rounded-r-[.92rem] md:rounded-l-none md:flex md:flex-col md:justify-center md:max-w-[32.5rem] md:py-desk md:px-desk'>
           <p className='mb-bottom flex text-justify pt-top p-inline'>Com essa célula de carga, é possível medir até 50kg, possibilitando criar balanças. Caso necessite fazer uma balança para até 100kg, basta utilizar 2 células de carga, 150kg 3 células de carga e assim sucessivamente, contanto que você divida a carga entre elas.</p>
           <p className='pb-top'>Preço: R$ 7,90</p>
         </div>
@@ -42,7 +42,7 @@ function Materiais() {
           <h2 className='text-titulo font-semibold pt-top text-xl md:text-center'>Módulo HX711</h2>
           <img src={modulo} alt="imagem de um módulo hx711" className='my-inline '/>
         </div>
-        <div className='bg-bgcaixa rounded-r-[.92rem] flex flex-col items-center md:flex md:flex-col md:justify-center md:max-w-[32.5rem] md:py-desk md:px-desk'>
+        <div className='bg-bgcaixa rounded-b-[.92rem] flex flex-col items-center md:rounded-r-[.92rem] md:rounded-l-none md:flex md:flex-col md:justify-center md:max-w-[32.5rem] md:py-desk md:px-desk'>
           <p className='mb-bottom flex text-justify pt-top p-inline'>Módulo conversor e amplificador de 24 bits, geralmente utilizado para amplificar sinais de células de carga (como, por exemplo, a Célula de Carga 50Kg), possibilitando a leitura através de um microntrolador como PIC, Arduino, Rasberry Pi, entre outros.</p>
           <p className='pb-top'>Preço: R$ 5,90</p>
         </div>
@@ -53,7 +53,7 @@ function Materiais() {
           <h2 className='text-titulo font-semibold pt-top text-xl md:text-center'>Protoboard</h2>
           <img src={protoboard} alt="imagem de uma protoboard" className='my-inline '/>
         </div>
-        <div className='bg-bgcaixa rounded-r-[.92rem] flex flex-col items-center md:flex md:flex-col md:justify-center md:max-w-[32.5rem] md:py-desk md:px-desk'>
+        <div className='bg-bgcaixa rounded-b-[.92rem] flex flex-col items-center md:rounded-r-[.92rem] md:rounded-l-none md:flex md:flex-col md:justify-center md:max-w-[32.5rem] md:py-desk md:px-desk'>
           <p className='mb-bottom flex text-justify pt-top p-inline'>Ela possui 2 barramentos para alimentação, 30 colunas e 10 linhas - um total de 400 pontos. Todos os pinos estão espaçados pelo padrão de 0,1". O espaçamento no meio é perfeito para componentes do tipo DIP. A protoboard aceita fios de 29 AWG até 20 AWG. Esta protoboard possui um adesivo auto colante. Você também pode ligar várias destas juntas e aumentar a área de prototipagem.</p>
           <p className='pb-top'>Preço: R$ 7,50</p>
         </div>
@@ -64,7 +64,7 @@ function Materiais() {
           <h2 className='text-titulo font-semibold pt-top text-xl md:text-center'>Workplate</h2>
           <img src={workplate} alt="imagem de uma workplate" className='my-inline '/>
         </div>
-        <div className='bg-bgcaixa rounded-r-[.92rem] flex flex-col items-center md:flex md:flex-col md:justify-center md:max-w-[32.5rem] md:py-desk md:px-desk'>
+        <div className='bg-bgcaixa rounded-b-[.92rem] flex flex-col items-center md:rounded-r-[.92rem] md:rounded-l-none md:flex md:flex-col md:justify-center md:max-w-[32.5rem] md:py-desk md:px-desk'>
           <p className='mb-bottom flex text-justify pt-top p-inline'>A WorkPlate 400 é uma base de trabalho para montar sua placa Arduino junto a uma protoboard de 400 furos, criando assim um ambiente ideal para o desenvolvimento de projetos de uma forma prática e dinâmica.</p>
           <p className='pb-top'>Preço: R$ 14,90</p>
         </div>
@@ -75,7 +75,7 @@ function Materiais() {
           <h2 className='text-titulo font-semibold pt-top text-xl md:text-center'>Display LCD</h2>
           <img src={lcd} alt="imagem de um display lcd" className='my-inline '/>
         </div>
-        <div className='bg-bgcaixa rounded-r-[.92rem] flex flex-col items-center md:flex md:flex-col md:justify-center md:max-w-[32.5rem] md:py-desk md:px-desk'>
+        <div className='bg-bgcaixa rounded-b-[.92rem] flex flex-col items-center md:rounded-r-[.92rem] md:rounded-l-none md:flex md:flex-col md:justify-center md:max-w-[32.5rem] md:py-desk md:px-desk'>
           <p className='mb-bottom flex text-justify pt-top p-inline'>Utilizando apenas 2 pinos de seu Arduino, ou qualquer outro microcontrolador que tenha suporte a comunicação I2C, você consegue escrever mensagens no LCD. Não só isso, usando o sistema de endereçamento de shift registers, você consegue ligar um LCD no outro através dos conectores LATCH, e usar até 8 LCDs deste tipo com os mesmos 2 pinos.</p>
           <p className='pb-top'>Preço: R$ 23,90</p>
         </div>
